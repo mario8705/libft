a2734c81808c9cc29e6571cc45e72ad3620b6e09
Alexis Lavaud <alavaud@student.le-101.fr>
2019-10-15 17:33:33 +0200

    Wow

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: alavaud <alavaud@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/07 15:42:06 by alavaud      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/11 18:29:43 by alavaud     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/15 17:32:12 by alavaud     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -17,7 +17,11 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char			*str;
 	unsigned int	i;
+	size_t			l;
 
+	l = ft_strlen(s);
+	if (start >= l)
+		return (ft_strdup(""));
 	str = (char *)malloc(len + 1);
 	i = 0;
 	if (str)

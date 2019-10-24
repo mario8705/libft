977b370b671312d4d2bff68d3d6d07e1f977e80d
Alexis Lavaud <alavaud@student.le-101.fr>
2019-10-24 17:10:48 +0200

    removed unused header include in ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,13 +6,12 @@
 /*   By: alavaud <alavaud@student.le-101.fr>        +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2019/10/07 13:28:24 by alavaud      #+#   ##    ##    #+#       */
-/*   Updated: 2019/10/23 16:19:59 by alavaud     ###    #+. /#+    ###.fr     */
+/*   Updated: 2019/10/24 17:10:18 by alavaud     ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <string.h>
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
